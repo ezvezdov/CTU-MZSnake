d2c4773687d1b45b1e3a30bcd77f8078ab9e85a1
Zviazdou, Yauheni <zviazyau@fel.cvut.cz>
2022-05-21 15:45:31 +0000

    Change snake speed to 1

--- options.c
+++ options.c
@@ -2,8 +2,8 @@
 
 game_t *init_game(){
   game_t *new_game = malloc(sizeof(game_t));
-  new_game->is_multiplayer = 1;
-  new_game->game_speed = 2;
+  new_game->is_multiplayer = 0;
+  new_game->speed = 1;
   new_game->is_border = 1;
   return new_game;
 }
