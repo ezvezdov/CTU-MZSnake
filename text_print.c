a3f31cc89c3aacc650f01a10f11ee5432cfd74be
Zviazdou, Yauheni <zviazyau@fel.cvut.cz>
2022-05-21 11:27:58 +0000

    Fixes

--- text_print.c
+++ text_print.c
@@ -1,14 +1,7 @@
-#include <string.h>
+#include "text_print.h"
+#include "screen_data.h"
 #include "font_types.h"
-#include <stdint.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <malloc.h>
-#include <string.h>
-#include <byteswap.h>
-#include <getopt.h>
-#include <inttypes.h>
-#include <time.h>
+
 
 font_descriptor_t *font = &font_winFreeSystem14x16;
 
@@ -34,7 +27,7 @@ int string_width(char *str){
 }
 
 
-void print_char(int x, int y, char ch, unsigned colour, int square, int **lcd_board) {
+void print_char(int x, int y, char ch, unsigned colour, int square, board_values **lcd_board) {
     uint16_t *ptr = font->bits + (ch - font->firstchar) *font->height;
     ch -= font->firstchar;
     for(int i = 0; i < font->height; i++){
@@ -50,9 +43,32 @@ void print_char(int x, int y, char ch, unsigned colour, int square, int **lcd_bo
     }
 }
 
-void print_string(int x, int y, char *str, unsigned colour, int square, int **lcd_board){
+void print_string(int x, int y, char *str, unsigned colour, int square, board_values **lcd_board){
   for(int i = 0; i < strlen(str); i++){
     print_char(x,y,str[i], colour,1, lcd_board);
     x+= char_width(str[i]) * square;
   }
+}
+
+void print_scores(int snake1_score, int snake2_score, board_values **lcd_board){
+  char str1[] = "Score:";
+  char snake1_count[10], snake2_count[10];
+  sprintf(snake1_count, "%d", snake1_score);
+  strcat(str1,snake1_count);
+  print_string(10,10, str1,SNAKE1,1,lcd_board);
+
+  char str2[] = "Score:";
+  sprintf(snake2_count, "%d", snake2_score);
+  strcat(str2,snake2_count);
+  print_string(100,10, str2,SNAKE2,1,lcd_board);
+}
+
+void print_timer(board_values **lcd_board, int msec){
+  char timer_mins[100], timer_secs[100];
+
+  sprintf(timer_mins, "%02d:", (msec)/60);
+  sprintf(timer_secs, "%02d", (msec)%60);
+  strcat(timer_mins,timer_secs);
+
+  print_string(SCREEN_X - string_width(timer_mins) - 10,10, timer_mins,TEXT,1, lcd_board);
 }
