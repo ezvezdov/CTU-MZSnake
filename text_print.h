5b995cac38aa0ac405e1a1f7b950cedd1826ea70
Zviazdou, Yauheni <zviazyau@fel.cvut.cz>
2022-05-21 11:28:13 +0000

    Fixes

--- text_print.h
+++ text_print.h
@@ -1,11 +1,24 @@
+#ifndef TEXT_PRINT_H
+#define TEXT_PRINT_H
+
+#include "snake.h"
+
 /*Check width of char ch.*/
 int char_width(int ch);
 
 /*Check width of string str.*/
 int string_width(char *str);
 
-/*print one char on screen.*/
-void print_char(int x, int y, char ch, unsigned colour, int square, int **lcd_board);
+/*Print one char on screen.*/
+void print_char(int x, int y, char ch, unsigned colour, int square, board_values **lcd_board);
 
-/* Print string on screen.*/
-void print_string(int x, int y, char *str, unsigned colour, int square, int **lcd_board);
+/*Print string on screen.*/
+void print_string(int x, int y, char *str, unsigned colour, int square, board_values **lcd_board);
+
+/*Print snakes info on screen*/
+void print_scores(int snake1_score, int snake2_score, board_values **lcd_board);
+
+/*Print timer data on screen*/
+void print_timer(board_values **lcd_board, int msec);
+
+#endif  /*TEXT_PRINT_H*/
